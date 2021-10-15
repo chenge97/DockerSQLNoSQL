c19f7070b256b412c7d429d59409f26ae7fe1162
chenge97 <chenge97@gmail.com>
2021-10-14 21:22:54 -0500

    Autocmmit set as true

--- App/__init__.py
+++ App/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('App.config.flask_config.FlaskConfig')
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app,session_options={'autocommit': True})
 
 
 import App.blueprints
